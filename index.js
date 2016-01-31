f7bc9c5d71bdb5aaea1735c776d6f793a7ee61af
Kelvin Del Monte <kdelmonten@gmail.com>
2016-01-30 22:26:16 -0500

    Allow the user to pass the ant path via a new parameter called options. If ant path is not passed, we will assume the user has it in their PATH. The new options parameter replaces the args one. Args may now be passed via options.args

--- index.js
+++ index.js
@@ -5,76 +5,70 @@
  * Released under the MIT license
  */
 
-
-exports.TMP_PATH = null;
-exports.ANT_PATH = './ant/bin/ant';
-
-// ---
+/*jshint multistr:true */
 
 var _childProcess = require('child_process');
 var _fs = require('fs');
 var _path = require('path');
 var _jxon = require('./lib/jxon');
+var os = require('os');
 
-// ---
-
-
-function exec(tasks, args, callback) {
+function exec(tasks, options, callback) {
     // args is optional
-    if ( arguments.length === 2 ) {
-        if (typeof args === 'function') {
-            callback = args;
-            args = [];
+    if (arguments.length === 2) {
+        if (typeof options === 'function') {
+            callback = options;
+            options = {};
         }
     }
-    args = args || [];
+    options.args = options.args || [];
 
-
-    var tmpFile = _path.join(exports.TMP_PATH, '_build_'+ uid() +'.xml');
-
-    /*jshint multistr:true */
+    var tmpFile = _path.join(os.tmpdir(), '_build_' + uid() + '.xml');
 
     var xml = '<?xml version="1.0" encoding="utf-8"?>\
 <project name="node_ant" default="run">\
   <target name="run">{{target}}</target>\
 </project>';
 
-    xml = xml.replace('{{target}}', _jxon.toXML(tasks) );
+    xml = xml.replace('{{target}}', _jxon.toXML(tasks));
 
+    function processCallback(e_err, stdout, stderr) {
+        _fs.unlink(tmpFile, function(u_err) {
+            if (typeof callback === 'function') {
+                var err = u_err || e_err;
+                callback(err, stdout, stderr);
+            }
+        });
+    }
 
     // write XML to temporary file and call ant passing it
-    _fs.writeFile(tmpFile, xml, 'utf-8', function(w_err){
+    _fs.writeFile(tmpFile, xml, 'utf-8', function(w_err) {
         if (w_err) {
             if (typeof callback === 'function') {
                 callback(w_err);
             }
-            // can't execute without writting the temp build file
+            // can't execute without writing the temp build file
             return;
         }
 
-        // windows uses the ".bat" file
-        var antPath = exports.ANT_PATH;
-        antPath += (process.platform === 'win32')? '.bat' : '';
-        antPath = _path.normalize(antPath);
-
-        _childProcess.execFile( antPath, ['-buildfile', tmpFile].concat(args), function(e_err, stdout, stderr) {
-            _fs.unlink(tmpFile, function(u_err){
-                if (typeof callback === 'function') {
-                    var err = u_err || e_err;
-                    callback(err, stdout, stderr);
-                }
-            });
-        });
+        var antPath;
+        if (options.antPath) {
+            var antExecutable = 'ant' + (process.platform === 'win32' ? '.bat' : '');
+            var antPath = _path.join(options.antPath, antExecutable);
+            antPath = _path.normalize(antPath);
+            _childProcess.execFile(antPath, ['-buildfile', tmpFile].concat(options.args), processCallback);
+        } else {
+            // Let's hope that ant is in the user's PATH
+            var cmd = ['ant', '-buildfile', tmpFile].concat(options.args);
+            _childProcess.exec(cmd, processCallback);
+        }
     });
-
 }
-
 
 var _prevUid = 0;
-function uid(){
-    return String(Date.now()).substr(-7) +'-'+ (++_prevUid);
+
+function uid() {
+    return String(Date.now()).substr(-7) + '-' + (++_prevUid);
 }
 
-
 exports.exec = exec;
-
